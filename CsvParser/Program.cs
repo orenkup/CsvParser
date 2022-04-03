8876a717a3bd89ebb2e9066e9044f60e11728052
orenkup <orenwantsyou@gmail.com>
2022-04-03 10:22:53 +0300

    Update Program.cs
    move the logic of printing the result from the engine to the program.

--- CsvParser/Program.cs
+++ CsvParser/Program.cs
@@ -1,10 +1,18 @@
-﻿
+
 // See https://aka.ms/new-console-template for more information
 
 
 var loader = new CsvLoader("CsvToProcess.csv", ",");
-loader.With(new FilterRows(3, "Iowa"))
-       .With(new GetColumn(9))
-       .With(new Avg())
+var result = loader.With(new FilterRows(3, "Iowa"))
+    .With(new GetColumn(9))
+    .With(new Avg())
     .Run();
-    ;
+
+
+foreach (var results in result)
+{
+    foreach (var str in results)
+    {
+        Console.WriteLine($"runing all parsers result:{ str}");
+    }
+}
